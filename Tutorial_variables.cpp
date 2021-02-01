7a70e0118e62d1c4d079be83daf5d1a5176dda57
chloenh <45791822+chloenh@users.noreply.github.com>
2021-02-01 15:14:40 +0100

    Create Tutorial_variables.cpp

---
+++ Tutorial_variables.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //integers is a number, type of variable
+    int test = 5;
+    int b = 3;
+    int c = test+b;
+    
+    // second type are booleans
+    
+    bool d = true; //=1
+    bool e = false; //= zero
+    
+    bool k =d&&e;
+    
+ //third type is double and float to store decimal
+    
+    double h  = 0.5;
+    float j = 0.5;
+    
+    double result = h+j;
+    
+    
+    cout<<"Hello everyone"<< result << endl;
+  
+    
+    return 0;
